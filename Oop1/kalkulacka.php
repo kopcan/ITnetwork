c03314861f3486e1c5399779869b3b9d21b98cd4
kopcan <kopcanj1@gmail.com>
2016-06-23 18:37:25 +0200

    Opravene oznamovanie nevyplneneho cisla pre uzivatela

--- Oop1/kalkulacka.php
+++ Oop1/kalkulacka.php
@@ -13,10 +13,12 @@
     </head>
     <body>
         <form action="" method="post" name="kalkulacka">
-            <p>1. číslo</p>
+            <p>1. číslo<br>
             <input type="text" name="cislo1">
-            <p>2. číslo</p>
+            </p>
+            <p>2. číslo<br>
             <input type="text" name="cislo2">
+            </p>
             <p>&nbsp;</p>
             <input type="submit" name="vypocitaj" value="Vypočítaj">
         </form>
@@ -24,13 +26,17 @@
         <p>
             <?php
                 if(isset($_POST['vypocitaj'])){
-                    var_dump($_POST['cislo1']);
-                    var_dump($_POST['cislo2']);
-                    if(!isset($cislo1) || ($cislo1 == '')){
-                        echo'Zabudli ste vložiť 1. číslo!';
-                    }
-                    elseif(!isset($cislo2) || ($cislo2 == '')){
-                        echo'Zabudli ste vložiť 2. číslo!';
+                    
+                    $checkNum1 = (!isset($cislo1) || ($cislo1 == '')) ? true : false;
+                    $checkNum2 = (!isset($cislo2) || ($cislo2 == '')) ? true : false;
+                    
+                    if($checkNum1 || $checkNum2){
+                        if($checkNum1){
+                        echo'Zabudli ste vložiť 1. číslo!<br>';                        
+                        }
+                        if($checkNum2){
+                        echo'Zabudli ste vložiť 2. číslo!<br>';                        
+                        }          
                     }else{
                         $vysledok = new Oop1_Pocitaj($cislo1, $cislo2);
                         echo "<hr><h3>Výsledok</h3>";
@@ -38,8 +44,9 @@
                         echo "Rozdiel: " . $vysledok->rozdiel() . "<br>";
                         echo "Súčin: " . $vysledok->sucin() . "<br>";
                         echo "Podiel: " . $vysledok->podiel() . "<br>";
+                        }
                     }
-                }
+                
             ?>
         </p>
     </body>
